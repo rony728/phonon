1d9f0d6b078b02f8c6f18c5a1428438f425e8e92
rony728 <rony728@gmail.com>
2018-02-05 17:02:12 -0600

    Commit.

---
+++ phonon.sh
@@ -0,0 +1,15 @@
+wget http://download.kde.org/stable/phonon/4.9.1/phonon-4.9.1.tar.xz                                                                              
+
+tar xf phonon-4.9.1.tar.xz                       
+cd phonon-4.9.1
+
+mkdir -p build &&
+cd    build &&
+
+cmake -DCMAKE_INSTALL_PREFIX=/usr    \
+      -DCMAKE_BUILD_TYPE=Release     \
+      -DPHONON_BUILD_PHONON4QT5=ON   \
+      -Wno-dev .. &&
+make
+                                                                                                                                                                                    
+#sudo make install
